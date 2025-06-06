9ce2313e888b930c42abd2943a5d17478ed014c7
Ethan Handschuh <ehandschuh@safetynetaccess.com>
2025-06-06 09:08:10 -0400

    Fix default ports

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     # Add parser arguments (switches)
     parser.add_argument("--host", "-ip", type=str, help="Enter 1 or more IP addresses or range of IP Addresses.", required=True)
-    parser.add_argument("--ports", '-p', type=str, help="Comma-serparated list or a range of ports.", required=False)
+    parser.add_argument("--ports", '-p', type=str, help="Comma-serparated list or a range of ports.", required=False, nargs="?", const="")
     parser.add_argument("--proto", "-pT", "-pU", type=str, choices=["tcp", "udp"], default="tcp", help="Choose transport, layer 4, protocol. Either tcp or udp.", required=False)
     parser.add_argument("--debug", action="store_true", help="Enables the debug logs.", required=False)
     parser.add_argument("--banner","-sB", action="store_true", help="Enables banner retrieval for TCP.", required=False)
@@ -24,7 +24,11 @@ if __name__ == "__main__":
     host_ips: list[str] = parse_host_ips(args.host)
     
     # Takes args.ports and adds them to a list or gets a range and add the ports within to the list
-    if args.ports: ports: list[str] = parse_ports(args.ports)
+    if args.ports == "":
+        with open("default_target_ports.txt", "r") as file:
+            ports: list[str] = file.read().replace(" ", "").split(",")
+    else:
+        ports: list[str] = parse_ports(args.ports)
     
     # Takes args.proto and determines if the user wants to use TCP or UDP transport protocol
     if args.proto: protocol: str = parse_protocol(args.proto)
